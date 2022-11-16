df27287e87df202cc61818317a4dff7b4af292f7
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2022-11-16 14:57:53 +0700

    feat: create class SlangDictionary

---
+++ src/SlangDictionary.java
@@ -0,0 +1,71 @@
+import java.util.List;
+
+/**
+ * PACKAGE_NAME
+ * Create by Đặng Ngọc Tiến
+ * Date 11/16/2022 - 2:47 PM
+ * Description: class to store slang words
+ */
+public class SlangDictionary {
+    private String slang;
+    private List<String> definition;
+
+    /**
+     * default constructor
+     */
+    public SlangDictionary(){
+        this.slang = "";
+        this.definition = null;
+    }
+
+    /**
+     * constructor with parameters
+     * @param slang: slang word
+     * @param definition: definition of slang word
+     */
+    public SlangDictionary(String slang, List<String> definition) {
+        this.slang = slang;
+        this.definition = definition;
+    }
+
+    /**
+     * copy constructor
+     * @param slangDictionary: slangDictionary object
+     */
+    public SlangDictionary(SlangDictionary slangDictionary){
+        this.slang = slangDictionary.slang;
+        this.definition = slangDictionary.definition;
+    }
+
+    /**
+     * get slang word
+     * @return slang word
+     */
+    public String getSlang() {
+        return slang;
+    }
+
+    /**
+     * set slang word
+     * @param slang: slang word
+     */
+    public void setSlang(String slang) {
+        this.slang = slang;
+    }
+
+    /**
+     * get definition of slang word
+     * @return definition of slang word
+     */
+    public List<String> getDefinition() {
+        return definition;
+    }
+
+    /**
+     * set definition of slang word
+     * @param definition: definition of slang word
+     */
+    public void setDefinition(List<String> definition) {
+        this.definition = definition;
+    }
+}
